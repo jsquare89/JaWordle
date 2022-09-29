8349f6286c72d785118a9d3d1fb4c0c098bf987f
Jarred J <jsquare89@users.noreply.github.com>
2022-09-28 22:24:30 -0700

    Update Key.js

--- src/components/Key.js
+++ src/components/Key.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Key(props){
     let value = props.obj.value
-    let keyStyle = "mx-0.5 py-4 rounded bg-slate-300 active:bg-slate-500 font-semibold select-none"
+    let keyStyle = "mx-0.5 py-4 rounded bg-slate-300 active:bg-slate-500 font-semibold text-sm select-none"
     if(value === "ENTER"){
-        keyStyle += " w-14 text-sm ml-0"
+        keyStyle += " w-14 text-xs ml-0"
     }else if(value === "DELETE" ){
-        keyStyle += " w-14 text-sm mr-0"
+        keyStyle += " w-14 text-xs mr-0"
     }else{
         keyStyle += " w-10"
     }
